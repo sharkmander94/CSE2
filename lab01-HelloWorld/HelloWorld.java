0d8cbb86aca08921cd30c5a02d8b1829bd30fa1a
sharkmander <jas517@lehigh.edu>
2016-01-29 16:53:57 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld{
+    
+    public static void main(String[] args) {
+//     print the thing in a box
+        System.out.println("#######################");
+        System.out.println("#######################");
+        System.out.println("#                     #");
+        System.out.println("#  Hello World 2016!! #");
+        System.out.println("#                     #");
+        System.out.println("#######################");
+        System.out.println("#######################");
+        System.out.println("test");
+        
+        }
+    }
